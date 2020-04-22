890dba896a439a14d9e93d622c77cb5ad3059960
zmalakhov <zm.13.15.zm@gmail.com>
2020-04-22 21:11:02 +0300

    01 - Установка

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="wrapper">
+        <div class="wrapper-content">
+
+            <section>
+                <div class="container">
+                    <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
+                    <!--First modal-->
+                    <modals
+                        title="First modal"
+                        v-if="modalFirst"
+                    ></modals>
+
+                </div>
+            </section>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import modals from '@/components/Modal.vue'
+
+    export default {
+        components: {modals},
+        data() {
+            return {
+                modalFirst: false
+            }
+        }
+    }
+</script>
+
+
