4dca133f7993b32781f44397301d0bf0ab5bf51e
zmalakhov <zm.13.15.zm@gmail.com>
2020-04-23 17:40:08 +0300

    07 - Базовая форма Переиспользуем модальное окно и базовая форма

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,40 @@
 
             <section>
                 <div class="container">
-                    <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
                     <!--First modal-->
+                    <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
                     <modals
                         title="First modal"
                         v-show="modalFirst"
-                        @close="modalFirst = !modalFirst"
+                        @close="modalFirst = false"
                     >
                         <!-- body -->
                         <div slot="body">
                             <p> Text Text Text Text Text Text Text </p>
                             <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Well dont!</button>
                         </div>
-
-
                     </modals>
+
+                    <!--Second modal-->
+                    <button class="btn btnPrimary" @click="modalSecond.show = !modalSecond.show">Show modal with form</button>
+                    <modals
+                            title="Modal with form"
+                            v-show="modalSecond.show"
+                            @close="modalSecond.show = false"
+                    >
+                        <!-- body -->
+                        <div slot="body">
+                            <form @submit.prevent="submitSecondForm">
+                                <label>Name:</label>
+                                <input type="text" required v-model="modalSecond.name">
+                                <label>Email:</label>
+                                <input type="email" required v-model="modalSecond.email">
+                                <button class="btn btnPrimary">Submit</button>
+                            </form>
+
+                        </div>
+                    </modals>
+
 
                 </div>
             </section>
@@ -34,7 +53,25 @@
         components: {modals},
         data() {
             return {
-                modalFirst: false
+                modalFirst: false,
+                modalSecond: {
+                    show: false,
+                    name: '',
+                    email: '',
+                },
+
+            }
+        },
+        methods:{
+            submitSecondForm(){
+                console.log({
+                    name: this.modalSecond.name,
+                    email: this.modalSecond.email,
+                })
+                this.modalSecond.name = ''
+                this.modalSecond.email = ''
+                this.modalSecond.show = false
+
             }
         }
     }
