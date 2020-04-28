9077ef23af59bfb727f910ffdea0d23f66801e9e
zmalakhov <zm.13.15.zm@gmail.com>
2020-04-28 18:04:14 +0300

    09 - Валидация #2 - Установка vuelidate, вывод ошибок, смена классов.

--- src/components/ModalValidate.vue
+++ src/components/ModalValidate.vue
@@ -6,10 +6,19 @@
         <!-- body -->
         <div slot="body">
             <form @submit.prevent="">
-                <label>Name:</label>
-                <input type="text" required>
+                <div class="form-item" :class="{ errorInput: $v.name.$error }">
+                    <label>Name:</label>
+                    <p class="errorText" v-if="!$v.name.required">Field is required!</p>
+                    <p class="errorText" v-if="!$v.name.minLength">Name must have at least {{
+                        $v.name.$params.minLength.min }} !</p>
+                    <input
+                            v-model="name"
+                            :class="{ error: $v.name.$error }"
+                            @change="$v.name.$touch()"
+                    >
+                </div>
                 <label>Email:</label>
-                <input type="email" required>
+                <input v-bind="email">
                 <button class="btn btnPrimary">Submit</button>
             </form>
 
@@ -19,12 +28,45 @@
 </template>
 
 <script>
+    import {required, minLength, email} from 'vuelidate/lib/validators'
+
     import modal from '@/components/UI/Modal.vue'
+
     export default {
-        components: { modal }
+        components: {modal},
+        data() {
+            return {
+                name: '',
+                email: ''
+            }
+        },
+        validations: {
+            name: {
+                required,
+                minLength: minLength(4)
+            },
+            email: {
+                required,
+                email
+            }
+        }
+
     }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+    .form-item .errorText {
+        display: none;
+        margin-bottom: 8px;
+        font-size: 13.4px;
+        color: #de4040;
+    }
+    .form-item {
+        &.errorInput .errorText{
+            display: block;
+        }
+    }
+    input.error {
+        border-color: #de4040;
+    }
 </style>
